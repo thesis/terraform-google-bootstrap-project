392ac7184e32921a5cf8307263e798224ee7a2d6
Kristen Borges <kb0rg@users.noreply.github.com>
2020-03-11 13:24:07 -0700

    Add disable arguments to the module, set defaults
    The disable_on_destroy default is set to the default documented in
    the resource
    The disable_dependent_services default is not documented in the
    resource, opting for false here since it will generate an error, on
    destroy, if any enabled services depend on this one

--- variables.tf
+++ variables.tf
@@ -35,6 +35,18 @@ variable "project_service_list" {
   default     = []
 }
 
+variable "disable_dependent_services" {
+  description = "Determines whether services that are enabled and which depend on this service should also be disabled when this service is destroyed (true/ false)."
+  type        = "string"
+  default     = "false"
+}
+
+variable "disable_on_destroy" {
+  description = "Determines whether to disable the service when the terraform resource is destroyed (true/ false)"
+  type        = "string"
+  default     = "true"
+}
+
 # Terraform backend bucket vars
 variable "location" {
   description = "The region to deploy the backend bucket in."
