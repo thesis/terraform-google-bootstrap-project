c50962d69b3cb9be260db6e22eb56d75e7cb73a2
Antonio Salazar Cardozo <antonio@thesis.co>
2022-08-16 16:48:27 -0400

    Migrate gpc_vpc module to unquoted variable types
    Unquoted types are required in modern terraform.

--- variables.tf
+++ variables.tf
@@ -1,20 +1,20 @@
 # global vars
 variable "labels" {
   description = "A list of key/value pairs to describe your resource.  Labels are akin to tags."
-  type        = "map"
+  type        = map
   default     = {}
 }
 
 # project resource vars
 variable "project_name" {
   description = "The name to give your project."
-  type        = "string"
+  type        = string
   default     = ""
 }
 
 variable "org_id" {
   description = "The ID for the organization the project will be created under. Local ENV VAR"
-  type        = "string"
+  type        = string
   default     = ""
 }
 
@@ -25,25 +25,25 @@ variable "billing_account" {
 
 variable "project_owner_members" {
   description = "A list of gcp users / service accounts to designate as owners of the project being created."
-  type        = "list"
+  type        = list
   default     = []
 }
 
 variable "project_service_list" {
   description = "List of services or APIs that will be enabled on project creation."
-  type        = "list"
+  type        = list
   default     = []
 }
 
 variable "disable_on_destroy" {
   description = "Determines whether to disable the service when the terraform resource is destroyed (true/ false)"
-  type        = "string"
+  type        = string
   default     = "true"
 }
 
 # Terraform backend bucket vars
 variable "location" {
   description = "The region to deploy the backend bucket in."
-  type        = "string"
+  type        = string
   default     = "us-east4"
 }
